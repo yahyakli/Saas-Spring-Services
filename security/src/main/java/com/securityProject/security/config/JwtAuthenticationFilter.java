91115bf8a118789a2d7a3cf4b13c787ded7fef4d
yahya akli <akli.yahya05@gmail.com>
2024-10-16 00:24:38 +0100

    java

--- security/src/main/java/com/securityProject/security/config/JwtAuthenticationFilter.java
+++ security/src/main/java/com/securityProject/security/config/JwtAuthenticationFilter.java
@@ -59,5 +59,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
+        filterChain.doFilter(request, response);
     }
 }
