b0adab0dc95f4cf2fd98cc329ce7ee622d7109a4
yahya akli <akli.yahya05@gmail.com>
2024-10-16 00:23:08 +0100

    java

--- security/src/main/java/com/securityProject/security/config/AplicationConfig.java
+++ security/src/main/java/com/securityProject/security/config/AplicationConfig.java
@@ -1,5 +1,23 @@
 package com.securityProject.security.config;
 
+
+import com.securityProject.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
 public class AplicationConfig {
-    
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+    }
 }
